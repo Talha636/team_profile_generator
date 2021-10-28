ac18e9e7aad7eec6f52ab8eac7b11c9fd46135a7
Talha636 <85548877+Talha636@users.noreply.github.com>
2021-10-27 22:26:18 -0400

    html styling complete

--- src/generateTeam.js
+++ src/generateTeam.js
@@ -7,48 +7,48 @@ let cards=``
 employees.forEach(employee => {
     if (employee.getRole() === 'Manager') {
         cards+= `
-        <div>
+        <div class='card col-3 m-3 rounded'>
+        <div class='card-body bg-primary'>
         <h1>${employee.name}</h1>
-        <h2>${employee.role}</h2>
+        <h2>☕️${employee.role}</h2>
         </div>
         <div>
         <h3>ID: ${employee.id}</h3>
         <h3>Email: <a href='mailto:${employee.email}'>${employee.email}</a></h3>
         <h3>Office Number: ${employee.getOfficeNumber().officeNumber}</h3>
         </div>
+        </div>
         `
     } else if (employee.getRole() === 'Engineer') {
         cards+= `
-        <div>
+        <div class='card col-3 m-3 rounded'>
+        <div class='card-body bg-primary'>
         <h1>${employee.name}</h1>
-        <h2>${employee.role}</h2>
+        <h2>🤓${employee.role}</h2>
         </div>
         <div>
         <h3>ID: ${employee.id}</h3>
         <h3>Email: <a href='mailto:${employee.email}'>${employee.email}</a></h3>
-        <h3>GitHub Username: <a href='https://github.com/${employee.getGithub().github}'>${employee.getGithub().github}</a></h3>
+        <h3>GitHub: <a href='${employee.getGithub().github}'>${employee.getGithub().github}</a></h3>
+        </div>
         </div>
         `
     } else {
         cards+= `
-        <div>
+        <div class='card col-3 m-3 rounded'>
+        <div class='card-body bg-primary'>
         <h1>${employee.name}</h1>
-        <h2>${employee.role}</h2>
+        <h2>🧑🏽‍🎓${employee.role}</h2>
         </div>
         <div>
         <h3>ID: ${employee.id}</h3>
         <h3>Email: <a href='mailto:${employee.email}'>${employee.email}</a></h3>
         <h3>School: ${employee.getSchool().school}</h3>
         </div>
+        </div>
         `
     }
-
-    
 });
-
-
-
-
 
     return `
     <!DOCTYPE html>
@@ -58,11 +58,20 @@ employees.forEach(employee => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel='stylesheet' href='style.css'>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <title>Project Team</title>
     </head>
     <body>
-    <header class='col-12'><h1>My Team</h1></header>
-        ${cards}
+    <header>
+    <h1>My Team</h1>
+    </header>
+    <div class='container'>
+        <div class='wrapper'>
+            <div class='row justify-content-center'>
+                ${cards}
+            </div>
+        </div>
+    </div>
     </body>
     </html>
     `
